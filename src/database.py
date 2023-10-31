a5bcaf8a2f4e4ec7bca46987e6366adc2eb5e5e6
Luke-Fanguna <lukefanguna@gmail.com>
2023-10-30 21:38:13 -0700

    V1 complete

--- src/database.py
+++ src/database.py
@@ -3,9 +3,8 @@ import dotenv
 from sqlalchemy import create_engine
 import sqlalchemy
 
-# def database_connection_url():
-#     dotenv.load_dotenv()
+def database_connection_url():
+    dotenv.load_dotenv()
+    return os.environ.get("POSTGRES_URI")
 
-#     return os.environ.get("POSTGRES_URI")
-
-# engine = create_engine(database_connection_url(), pool_pre_ping=True)
+engine = create_engine(database_connection_url(), pool_pre_ping=True)
