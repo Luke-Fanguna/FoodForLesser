5d9aa30db579ff0778118c6d560be45333d975c5
Luke-Fanguna <lukefanguna@gmail.com>
2023-11-03 14:02:53 -0700

    format

--- src/api/search.py
+++ src/api/search.py
@@ -8,3 +8,15 @@ tags=["list"],
 dependencies=[Depends(auth.get_api_key)],
 )
 
+@router.post("/stores/{list_id}/distribute (POST)")
+def distribute_list():
+    """ """
+
+
+@router.post("/stores/{list_id}/best (POST)")
+def find_best_item():
+    """ """
+
+@router.post("/stores/ (GET)")
+def find_stores():
+    """ """
