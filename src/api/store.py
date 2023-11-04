48df12404e1eea07cb250a0548e149a9806c9a38
Luke-Fanguna <lukefanguna@gmail.com>
2023-11-04 16:24:51 -0700

    added distribute_list logic, needs to test

--- src/api/store.py
+++ src/api/store.py
@@ -10,9 +10,54 @@ tags=["list"],
 dependencies=[Depends(auth.get_api_key)],
 )
 
+# gets the list_id and then checks which stores have the best price
+# list_id 
+# get item_id with list_id from grocery_list_items
+# with item_id, find the smallest for each value and unique values are found with list_id
+# list_id == user_id
 @router.post("/stores/{list_id}/distribute (POST)")
 def distribute_list(list_id: int):
     """ """
+    with db.engine.begin() as connection:
+        # gets every item from list_id
+        items = connection.execute(sqlalchemy.text(
+        """
+        SELECT
+            item_id
+        WHERE list_id = :list_id
+        """
+        ),[{"list_id":list_id}]).fetchall()
+        if items[0] == None:
+            return []
+        items = list(items)
+        best = []
+        
+        # searches crowdsources entries for best price
+        for item in items:
+            best = connection.execute(sqlalchemy.text(
+            """
+            SELECT 
+                store_id,
+                MIN(price)
+            FROM crowdsourced_entries
+            WHERE item_id = :item_id
+            ORDER BY store_id asc;
+            """
+            ),[{"item_id":item}]).fetchall()
+            
+            distribute = []
+            # joins best prices items with their stores
+            distribute.append([connection.execute(sqlalchemy.text(
+            """
+            SELECT
+                name
+            FROM stores
+            WHERE store_id = :store_id
+            """
+            ),[{"store_id":best[0]}]).fetchone()[0],best[1]])
+        
+        return distribute
+        
 
 
 @router.post("/stores/{list_id}/best (POST)")
