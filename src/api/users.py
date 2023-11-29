89017b90e527e61a3279b9699ed1b3195e2b1421
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2023-11-28 18:00:21 -0800

    user with concurrency

--- src/api/users.py
+++ src/api/users.py
@@ -13,37 +13,31 @@ router = APIRouter(
 @router.post("/add")
 def create_user(username : str, email : str):
     if not username and not email:
-        return 'EMPTY USERNAME/EMAIL'
+        return {"result": "EMPTY USERNAME/EMAIL" }
     calpoly = re.compile('[A-Za-z]+@calpoly\.edu')
     gmail = re.compile('[A-Za-z]+@gmail\.com')
 
     if calpoly.match(email) is None and gmail.match(email) is None:
-        return 'MUST USE CALPOLY OR GMAIL EMAIL ADDRESS'
+        return {"result": "MUST USE CALPOLY OR GMAIL EMAIL ADDRESS" }
     
     with db.engine.begin() as connection:
         # check if the user already exists
         entry = connection.execute(sqlalchemy.text(
         '''
-        SELECT
-        *
-        FROM users
-        WHERE username LIKE :username OR email LIKE :email;
-        ''')
-        ,[{'username':username, 'email':email}]).scalar()
-
-        if entry:
-            return 'Username or Email already in use'
-       
-       # if it does not exist, add to db
-        id = connection.execute(sqlalchemy.text(
-        '''
-        INSERT INTO users
-        (username, email)
-        VALUES
-        (:username,:email)
-        RETURNING id
+            WITH check_existing AS (
+                SELECT id
+                FROM users
+                WHERE username = :username OR email = :email
+            )
+            INSERT INTO users (username, email)
+            SELECT :username, :email
+            WHERE NOT EXISTS (SELECT 1 FROM check_existing)
+            RETURNING id;
         '''    
         )
         ,[{'username':username,'email':email}]).scalar()
+
+        if entry is None:
+            return { "result" : "Username or Email already in use" }
         
-        return { 'user_id' : id }
+        return { 'user_id' : entry }
